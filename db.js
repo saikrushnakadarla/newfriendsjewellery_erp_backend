0571ed94325546a45e560453193384488306345a
pavanimyana123 <pavanimyana2000@gmail.com>
2025-12-17 16:50:43 +0530

    Pavani

--- db.js
+++ db.js
@@ -3,19 +3,10 @@ const mysql = require('mysql2');
 const db = mysql.createPool({
   host: 'localhost', 
   user: 'root', 
-  password: 'Pavani@123', 
-  database: 'react_db',
-  port: 3307,
+  password: '', 
+  database: 'newfriendsjewellery_erp',
 });
 
-
-// const db = mysql.createPool({
-//   host: 'localhost', 
-//   user: 'root', 
-//   password: 'ksk@1005', 
-//   database: 'react_db',
-//   port: 3306,
-// });
 
 db.getConnection((err, connection) => {
   if (err) {
